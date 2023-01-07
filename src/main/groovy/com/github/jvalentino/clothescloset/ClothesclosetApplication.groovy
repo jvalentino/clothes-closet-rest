286b83f1bc915b3b0a8340e55be3c46ee7a1084c
jvalentino <jvalentino2@gmail.com>
2023-01-07 09:48:50 -0600

    got the thing to run

---
+++ src/main/groovy/com/github/jvalentino/clothescloset/ClothesclosetApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.jvalentino.clothescloset
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ClothesclosetApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ClothesclosetApplication, args)
+	}
+
+}
